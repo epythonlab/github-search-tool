54f7a7c30e3dc7b73dd06109531e27f9bff11715
Dejene Techane <dtechane6@gmail.com>
2023-06-17 07:01:25 -0400

    pylint errors fixed at app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-# app.js
-# import flask modules
+"""
+	File name:  app.py
+	In this app.py file, you have to define app
+	and import the routes and register the blueprint
+	import Flask module
+"""
 
-from flask import *
+from flask import Flask
 
 # construct the code app objects
 app = Flask(__name__)
@@ -11,7 +15,6 @@ app.config.from_object('settings.Config')
 
 with app.app_context():
 	from routes import index
-
 	# register the index route 
 	app.register_blueprint(index.index_bp)
 
